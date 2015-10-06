790a6d307bc37dddc89ec1e7510fde9c57827fe2
Michal Špaček <tupinek@gmail.com>
2015-10-06 09:23:08 +0200

    Improve SEE ALSO section in doc.

--- EN.pm
+++ EN.pm
@@ -3044,13 +3044,13 @@ L<Readonly>.
 
 =head1 SEE ALSO
 
-L<Mock::Person>,
-L<Mock::Person::CZ>,
-L<Mock::Person::DE>,
-L<Mock::Person::SK>,
-L<Mock::Person::SK::ROM>,
-L<Mock::Person::SV>,
-L<Mock::Person::RU>.
+=over
+
+=item L<Task::Mock::Person>
+
+Install the Mock::Person modules.
+
+=back
 
 =head1 REPOSITORY
 
