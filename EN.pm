0db55cd7b29e7c10b8af120847f79d21983c69b0
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-27 17:34:59 +0100

    Prepare for 0.06 version

--- EN.pm
+++ EN.pm
@@ -17,7 +17,7 @@ Readonly::Array our @EXPORT_OK => qw(first_male first_female middle_female
 # Variables.
 our $TYPE = 'two';
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # First and middle male names.
 our @first_male = our @middle_male = qw(
@@ -3068,6 +3068,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
--- t/Mock-Person-EN/02-version.t
+++ t/Mock-Person-EN/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($Mock::Person::EN::VERSION, 0.05, 'Version.');
+is($Mock::Person::EN::VERSION, 0.06, 'Version.');
